9e924e0212335b665d4d7e144e454f52cb3b1618
Sandi Metz <sandi@sandimetz.com>
2014-11-14 08:53:35 -0600

    bottles really with data clump

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,5 +1,18 @@
-class Bottles
+module BottleNumberFactory
+  refine Fixnum do
+    def to_bottle_number
+      begin
+        Object.const_get("BottleNumber#{self}")
+      rescue
+        BottleNumber
+      end.new(self)
+    end
+  end
+end
 
+using BottleNumberFactory
+
+class Bottles
   def song
     verses(99, 0)
   end
@@ -9,49 +22,64 @@ class Bottles
   end
 
   def verse(number)
-    "#{amount(number).capitalize} #{container(number)} of beer on the wall, " +
-    "#{amount(number)} #{container(number)} of beer.\n" +
-    "#{action(number)}, " +
-    "#{amount(successor(number))} #{container(successor(number))} of beer on the wall.\n"
+    bottle_number      = number.to_bottle_number
+    next_bottle_number = bottle_number.successor
+    "#{bottle_number.amount.capitalize} #{bottle_number.container} of beer on the wall, " +
+    "#{bottle_number.amount} #{bottle_number.container} of beer.\n" +
+    "#{bottle_number.action}, " +
+    "#{next_bottle_number.amount} #{next_bottle_number.container} of beer on the wall.\n"
   end
 
-  def container(number)
-    if number == 1
-      "bottle"
-    else
-      "bottles"
-    end
+end
+
+class BottleNumber
+  attr_reader :number
+
+  def initialize(number)
+    @number = number
   end
 
-  def pronoun(number)
-    if number == 1
-      "it"
-    else
-      "one"
-    end
+  def container
+    "bottles"
   end
 
-  def amount(number)
-    if number == 0
-      "no more"
-    else
-      number.to_s
-    end
+  def pronoun
+    "one"
   end
 
-  def action(number)
-    if number == 0
-      "Go to the store and buy some more"
-    else
-      "Take #{pronoun(number)} down and pass it around"
-    end
+  def amount
+    number.to_s
   end
 
-  def successor(number)
-    if number == 0
-      99
-    else
-      number - 1
-    end
+  def action
+    "Take #{pronoun} down and pass it around"
+  end
+
+  def successor
+    (number - 1).to_bottle_number
+  end
+end
+
+class BottleNumber0 < BottleNumber
+  def amount
+    "no more"
+  end
+
+  def action
+    "Go to the store and buy some more"
+  end
+
+  def successor
+    99.to_bottle_number
+  end
+end
+
+class BottleNumber1 < BottleNumber
+  def container
+    "bottle"
+  end
+
+  def pronoun
+    "it"
   end
 end
