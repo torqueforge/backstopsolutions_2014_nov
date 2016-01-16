bb207c58823eacf5a676a6ccab5cf6204bdcc4d7
Sandi Metz <sandi@sandimetz.com>
2016-01-16 08:18:03 -0500

    add copyright and license

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -1,4 +1,17 @@
 class House
+  DATA = [
+    "the horse and the hound and the horn that belonged to",
+    "the farmer sowing his corn that kept",
+    "the rooster that crowed in the morn that woke",
+    "the priest all shaven and shorn that married",
+    "the man all tattered and torn that kissed",
+    "the maiden all forlorn that milked",
+    "the cow with the crumpled horn that tossed",
+    "the dog that worried",
+    "the cat that killed",
+    "the rat that ate",
+    "the malt that lay in",
+    "the house that Jack built"].shuffle
 
   def recite
     1.upto(12).collect {|i| line(i)}.join("\n")
@@ -9,21 +22,8 @@ class House
   end
 
   def phrase(num)
-    [ "the horse and the hound and the horn that belonged to",
-      "the farmer sowing his corn that kept",
-      "the rooster that crowed in the morn that woke",
-      "the priest all shaven and shorn that married",
-      "the man all tattered and torn that kissed",
-      "the maiden all forlorn that milked",
-      "the cow with the crumpled horn that tossed",
-      "the dog that worried",
-      "the cat that killed",
-      "the rat that ate",
-      "the malt that lay in",
-      "the house that Jack built"].last(num).join(" ")
+    DATA.last(num).join(" ")
   end
 end
 
-puts
-puts House.new.line(12)
-puts
+puts House.new.recite
